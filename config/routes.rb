78a32ecf0d9e2602e4d66ed126a61607a1c9f26e
Zach Toolson <zach.toolson@gmail.com>
2015-06-25 09:33:03 -0600

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  # Routes added my monban
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+
+  root 'homes#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
