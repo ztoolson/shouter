9f07d3da2d32b3fd966b6bdefb14d7c289f01b78
Zach Toolson <zach.toolson@gmail.com>
2015-06-25 09:52:43 -0600

    Redirect user to a dashboard homepage once logged in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root 'homes#show'
+
+  resource :dashboard, only: [:show]
+
   # Routes added my monban
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 
-  root 'homes#show'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
